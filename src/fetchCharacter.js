7f9a1ef649fbc351adc488b5dd36fef779d363d8
Otávio <otaviotardellii@gmail.com>
2022-02-01 18:17:21 -0300

    Exercise 1 is done

--- src/fetchCharacter.js
+++ src/fetchCharacter.js
@@ -16,6 +16,28 @@ const fetchCharacter = async (id) => {
   }
 };
 
+it('verifica o nome do personagem', async () => {
+  const charater = await fetchCharacter('720');
+  expect(charater.name).toBe('Wonder Woman');
+})
+
+it('Verifica se retorna erro ao executar uma função sem parâmetro', async () => {
+  const fail = await fetchCharacter();
+  expect(fail).toEqual(new Error ('You must provide an url'))
+})
+
+it('Verifica se retorna erro ao executar função com parâmetro que não existe', async () => {
+  const response = await fetchCharacter('teste parametro errado')
+  expect(response).toBe('Invalid id');
+})
+
+it('verifica se a URL é correta', async () => {
+  const url = 'https://www.superheroapi.com/api.php/4192484924171229/720';
+  await fetchCharacter('720');
+  expect(fetch).toHaveBeenCalledTimes(4);
+  expect(fetch).toHaveBeenCalledWith(url);
+})
+
 // const fetchCharacter = async (id) => {
 //   const URL = `https://www.superheroapi.com/api.php/4192484924171229/${id}`;
 //   const promise = fetch(URL)
